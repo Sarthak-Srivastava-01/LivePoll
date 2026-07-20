7484882dabea85173143fe8b454389849a101b96
Sarthak-Srivastava-01 <sarthak.srivastava1398@gmail.com>
2026-07-20 16:04:17 +0530

    updation to connection

--- backend/connection.js
+++ backend/connection.js
@@ -1,12 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const url = 'mongodb+srv://sarthaksrivastava1398:sartmongo@cluster0.zylkuuk.mongodb.net/livepolldb?retryWrites=true&w=majority&appName=Cluster0';
-
-mongoose.connect(url)
-.then((result) => {
-    console.log('Connected to mongodb');    
-}).catch((err) => {
-    console.log(err);    
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
 
 module.exports = mongoose;
